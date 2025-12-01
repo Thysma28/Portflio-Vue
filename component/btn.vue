f8b34d8916a446d4058e032b7f77d3118f7a5484
LEININGER Mathys <156470119+Thysma28@users.noreply.github.com>
2025-12-01 13:43:49 +0100

    feat: initial commit

---
+++ component/btn.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps({
+  Name: {
+    type: String,
+    default: "#"
+  },
+  Link: {
+    type: String,
+    default: "#"
+  }
+})
+</script>
+
+<template>
+  <a class="fancy-btn" :href="props.Link">
+    {{ props.Name }}
+  </a>
+</template>
+
+<style scoped>
+
+.fancy-btn {
+    background-color: white;
+    color: black;
+    font-weight: bold;
+    padding: 12px 24px;
+    border-radius: 12px; /* bords arrondis */
+    border: none;
+    cursor: pointer;
+    transition: transform 0.2s ease, box-shadow 0.2s ease;
+    font-size: 16px;
+}
+
+.fancy-btn:hover {
+    transform: scale(1.05); /* grandit de 5% */
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+}
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Btn Name="Github" Link="https://about.arffornia.com/About"/>
+</template>
+
+<script setup>
+import { computed,ref } from 'vue';
+import Btn from '../component/btn.vue';
+</script>
